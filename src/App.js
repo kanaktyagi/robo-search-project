ec4ecce88c80db7f6e42c803eb120061567a799f
Kanak Tyagi <tyagiknk.india@gmail.com>
2022-07-30 14:29:30 +0530

    Added the filter logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state={
+    monsters:[],
+    filteredMonsters: []
+    };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState(() =>  {
+      return { monsters: users, filteredMonsters: users}}, 
+      () => console.log(this.state.monsters)))
+  }
+  render() {
+    return (
+      <>
+      <div>
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
+          const searchString = event.target.value.toLocaleLowerCase();
+           const filterMonsters = this.state.monsters.filter (monster => monster.name.toLocaleLowerCase().includes(searchString))
+          console.log("onChange called")
+           this.setState ( () => {
+            return {
+              filteredMonsters : filterMonsters
+            }
+           })
+        }}/>
+        {this.state?.filteredMonsters.map(monster => {
+        return (
+          <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+        )
+      })}</div>
+      </>
+    )
+  }
 }
-
-export default App;
